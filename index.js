0436dba31a694789c42e9d5055e0560626275407
doreamon-git <abtiwari2904@gmail.com>
2022-08-20 19:35:53 +0530

    add start script

--- index.js
+++ index.js
@@ -164,8 +164,9 @@ app.post('/reviews/draft',isLoggedIn, async (req, res)=>{
     res.redirect('/reviews')
 })
 
-app.listen(3000, ()=> {
-    console.log("Listening on port 3000")
+const port = process.env.PORT || 3000
+app.listen(port, ()=> {
+    console.log(`Listening on port ${port}`)
 })
 
 
